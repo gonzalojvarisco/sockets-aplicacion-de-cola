f9efa2161e81e217b570a7294657162b55832f20
gonzalo <gonzalojvarisco@gmail.com>
2019-04-22 11:54:10 -0300

    aplicacion finalizada

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -15,7 +15,8 @@ io.on('connection', (client) => {
     });
 
     client.emit('estadoActual', {
-        actual: ticketControl.getUltimoTicket()
+        actual: ticketControl.getUltimoTicket(),
+        ultimos4: ticketControl.getUltimos4()
     });
 
     client.on('atenderTicket', (data, callback) => {
@@ -30,6 +31,12 @@ io.on('connection', (client) => {
         let atenderTicket = ticketControl.atenderTicket(data.escritorio);
 
         callback(atenderTicket);
+
+        //actualizar/notificar cambios en los ultimos4
+
+        client.broadcast.emit('ultimos4', {
+            ultimos4: ticketControl.getUltimos4()
+        });
 
     });
 
