a0884c77a76ef5818211d3468063fe482d2cc105
Gonzalo Varisco <gonzalojvarisco@gmail.com>
2019-04-22 01:10:02 -0300

    avances atender un ticket

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -7,15 +7,30 @@ const ticketControl = new TicketControl();
 
 io.on('connection', (client) => {
 
-    client.on('siguienteTicket', (data, callback)=>{
+    client.on('siguienteTicket', (data, callback) => {
         let siguiente = ticketControl.siguiente();
 
         console.log(siguiente);
         callback(siguiente);
     });
 
-    client.emit('estadoActual',{
+    client.emit('estadoActual', {
         actual: ticketControl.getUltimoTicket()
+    });
+
+    client.on('atenderTicket', (data, callback) => {
+
+        if (!data.escritorio) {
+            return callback({
+                err: true,
+                mensaje: 'El escritorio es necesario'
+            });
+        }
+
+        let atenderTicket = ticketControl.atenderTicket(data.escritorio);
+
+        callback(atenderTicket);
+
     });
 
 });
